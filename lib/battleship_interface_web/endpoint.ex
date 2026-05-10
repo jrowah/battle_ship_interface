173470ab0a2c968d46904e3cc625de9c76823598
Rowa <jrowah@gmail.com>
2026-05-10 22:52:50 +0300

    add channel to persist game connections

--- lib/battleship_interface_web/endpoint.ex
+++ lib/battleship_interface_web/endpoint.ex
@@ -11,6 +11,10 @@ defmodule BattleshipInterfaceWeb.Endpoint do
     same_site: "Lax"
   ]
 
+  socket "/socket", BattleshipInterfaceWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
